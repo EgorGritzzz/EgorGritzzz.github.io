c4215d7ad126a72490666b07c847907896f97a90
EgorGritzzz <134557979+EgorGritzzz@users.noreply.github.com>
2024-08-14 11:42:40 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// Скрипт на кнопку бургер
+
+let burgerBtn = document.querySelector('.header__burger-btn');
+let navList = document.querySelector('.navlist_hidden');
+
+burgerBtn.addEventListener('click', ()=>{
+    navList.style.display = navList.style.display==='none'?'block':'none';
+})
+
+
+// Обновление заголовка
+
+fetch("https://baconipsum.com/api/?type=lucky", { method: 'GET' })
+.then(res => res.json())
+.then(response => {
+    console.log(response);
+    const text = document.querySelector('.description_comment p');
+    text.innerText = response;
+})
+.catch(err => {
+    console.log("error" + err);
+});
